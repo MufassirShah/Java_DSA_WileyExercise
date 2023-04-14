26390fa5dcc201cfd3653fc58808f7637c99a9a7
MufassirShah <shahmufassir4@gmail.com>
2023-04-14 12:20:59 +0500

    Problem 5

---
+++ ReinforcementP5.java
@@ -0,0 +1,21 @@
+/* Write a short Java method that takes an integer n and return the sum of the square all even positive integers less than or equal to n */
+import java.util.Scanner;
+
+public class ReinforcementP5 {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        System.out.println("Please Enter The value: ");
+        n = scanner.nextInt();
+        scanner.close();
+        System.out.println(inputMethod(n));
+    }
+    public static int inputMethod(int n){
+        int sum=0;
+        for(int i=2; i<=n; i+=2){
+            sum+=Math.pow(i,2);
+        }
+        return sum;
+    }
+    
+}
